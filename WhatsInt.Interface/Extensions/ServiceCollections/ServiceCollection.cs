214e997b2848aabf43e9395d21cf6f1866479559
Andercon <anderconprado@agger.com.br>
2022-07-22 16:49:52 -0300

    Added QuestionModule.

--- WhatsInt.Interface/Extensions/ServiceCollections/ServiceCollection.cs
+++ WhatsInt.Interface/Extensions/ServiceCollections/ServiceCollection.cs
@@ -37,6 +37,8 @@ namespace WhatsInt.Interface.Extensions.ServiceCollections
 
             serviceCollection.AddSingleton<UserService>();
 
+            serviceCollection.AddSingleton<QuestionService>();
+
             serviceCollection.AddSingleton<AuthorizationService>();
 
             serviceCollection.AddSingleton<IDatabaseSettings>(sp => sp.GetRequiredService<IOptions<DatabaseSettings>>().Value);
---
+++ WhatsInt.Interface/Services/QuestionService.cs
@@ -0,0 +1,32 @@
+﻿using Infrastructure.Repository;
+using System.Net;
+using WhatsInt.Infrastructure.Entities;
+using WhatsInt.Interface.Exceptions;
+using WhatsInt.Interface.Helpers;
+using WhatsInt.Model;
+
+namespace WhatsInt.Interface.Services
+{
+    public class QuestionService
+    {
+        private readonly IRepository<Question> _questionRepository;
+        private readonly IHttpContextAccessor _context;
+
+        public QuestionService(IRepository<Question> questionRepository, IHttpContextAccessor context)
+        {
+            _questionRepository = questionRepository;
+            _context = context; 
+        }
+
+        internal async Task<QuestionDto?> FindQuestionById(string id)
+        {
+            var questionFound = await _questionRepository.FindOne(x => x.Id == id);
+
+            if (questionFound == null)
+                throw new AppException(HttpStatusCode.NotFound, "Question not found");
+
+            return MapperHelper.Map<QuestionDto?>(questionFound); 
+        }
+    }
+
+}
