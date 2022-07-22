1d90bc86aa409531587d1ffc05d54b6bdb7826d6
VictorCovre <victorcovre@agger.com.br>
2022-07-22 14:12:19 -0300

    Update LoginView HTML Complete

--- WhatsInt/Views/LoginView.razor.cs
+++ WhatsInt/Views/LoginView.razor.cs
@@ -2,13 +2,40 @@
 {
     public partial class LoginView
     {
-        private string user;
+        private string userMail;
+        private string userName;
         private string password;
+        private string passwordConfirmation;
 
+        private string _labelCadasterText;
+        public string LabelCadasterText = "Ainda não possui cadastro? Cadastre-se Aqui";
+
+        private bool onLogin;
+
+        private string labelButtonLogin = "Log In";
 
         public void LoginButtonClick()
         {
 
+        }
+
+        public void CadasterLabelClick()
+        {
+            onLogin = !onLogin;
+            switch (LabelCadasterText)
+            {
+                case "Ainda não possui cadastro? Cadastre-se Aqui":
+                    LabelCadasterText = "Ja possui cadastro? Clique aqui para logar";
+                    labelButtonLogin = "Cadastrar";
+                    _labelCadasterText = LabelCadasterText;
+                    return;
+
+                case "Ja possui cadastro? Clique aqui para logar":
+                    LabelCadasterText = "Ainda não possui cadastro? Cadastre-se Aqui";
+                    labelButtonLogin = "Log In";
+                    _labelCadasterText = LabelCadasterText;
+                    return;
+            }
         }
     }
 }
